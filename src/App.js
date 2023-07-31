9621aae5dc5f4a139f7190e7ab13a27234c140fe
Prem <ppremkumar96@gmail.com>
2023-07-31 22:34:22 +0530

    react-projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [data,setData] = useState({userName:"",password:"",})
+  const [focusvar,setfocusvar]=useState(false);
+  
+  
+  const {userName,password}=data;
+
+  const onChange=(e)=>{
+
+    setData({...data,[e.target.name]:e.target.value})
+  }
+
+  // const showMessage=()=>setfocusvar(true)
+  // const dontDhowMessage=()=>setfocusvar(false)
+
+
+  const evnetHandler=(e)=>{
+    e.preventDefault();
+    console.log(data)
+  }
+
+  return ( 
+    <center>
+    <form onSubmit={evnetHandler}>
+      <input type="text" placeholder="enter username" name="userName" value={userName} onChange={onChange}/><br/>
+      <input type="password" placeholder="enter password"  name="password" value={password} onFocus={()=>setfocusvar(true)} onBlur={()=>setfocusvar(false)} onChange={onChange}/><br/>
+      {(password.length<=5 && focusvar===true) ? <p>Password should be greater than 5 characters</p> : null }
+      <button type="submit">submit</button>
+      </form>
+      </center>
+   );
 }
-
-export default App;
+export default App
